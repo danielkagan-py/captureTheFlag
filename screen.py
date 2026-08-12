ee3b7f2a8941b099384a7b2ac7e0be219a9f04d5
daniel <danielkagan00@gmail.com>
2026-08-12 12:55:10 +0300

    h

--- screen.py
+++ screen.py
@@ -41,5 +41,43 @@ def display():
                     randomW = random.randrange(0 ,consts.WINDOW_WIDTH)
                     res.show(randomH,randomW)
         pygame.display.flip()
-if __name__ == "__main__":
-     display()
+# if __name__ == "__main__":
+#      display()
+
+
+import sys, pygame, math, time;
+from pygame.locals import *;
+spaceship = ('pics/grass.png.png')
+mouse_c = ('pics/flag.png.png')
+backg = ('pics/snake.png')
+fire_beam = ('pics/mine.png')
+pygame.init()
+screen = pygame.display.set_mode((800, 600))
+bk = pygame.image.load(backg).convert_alpha()
+mousec = pygame.image.load(mouse_c).convert_alpha()
+space_ship = pygame.image.load(spaceship).convert_alpha()
+f_beam = pygame.image.load(fire_beam).convert_alpha()
+clock = pygame.time.Clock()
+pygame.mouse.set_visible(False)
+space_ship_rect = space_ship.get_rect()
+while True:
+    screen.blit(bk, (0, 0))
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
+        elif event.type == MOUSEBUTTONDOWN and event.button == 1:
+            print("Left Button Pressed")
+        elif event.type == MOUSEBUTTONDOWN and event.button == 3:
+            print("Right Button Pressed")
+        if event.type == MOUSEMOTION:
+            x1, y1 = pygame.mouse.get_pos()
+            x2, y2 = space_ship_rect.x, space_ship_rect.y
+            dx, dy = x2 - x1, y2 - y1
+            rads = math.atan2(dx, dy)
+            degs = math.degrees(rads)
+            pygame.transform.rotate(space_ship, degs)
+            pygame.display.update()
+    pos = pygame.mouse.get_pos()
+    screen.blit(mousec, (pos))
+    pygame.display.update()
