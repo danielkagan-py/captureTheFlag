2e5fc5c520ec6e2a2ff02ea5e3b4f4e81ceef53f
danielkagan-py <danielkagan00@gmail.com>
2026-08-17 11:46:28 +0300

    Update screen.py

--- screen.py
+++ screen.py
@@ -3,41 +3,36 @@ import pygame
 import consts
 #---------------------------------------------------------------------------------------------------------------------------------------
 MINE_IMG = "pics/mine.png"
+field1 = game_field.create_Xray_field()
+screen1 = pygame.display.set_mode((consts.screen_w, consts.screen_h))
 #---------------------------------------------------------------------------------------------------------------------------------------
 def show_mines():
-   screen = pygame.display.set_mode((consts.screen_w, consts.screen_h))
-   screen.fill((consts.black))
-   mainLoop = True
-   while mainLoop:
-       for event in pygame.event.get():
-           if event.type == pygame.QUIT:
-               mainLoop = False
-           i = 0
-           for col in range (consts.COL):
-               for row in range (consts.ROWS):
-                   pygame.draw.line(screen, consts.green, (0 + i, 0 ), ( 0 +i,999999 ))
-                   pygame.draw.line(screen, consts.green, (0 , 0 + i), ( 99999 , 0 + i))
-                   i += 28
-       pygame.display.update()
-   pygame.quit()
+  screen1.fill(consts.black)
+  i = 0
+  for row in range (consts.matriz_rows):
+      for col in range (consts.matriz_cols):
+          pygame.draw.line(screen1, consts.green, (i, 0), ( i, consts.screen_h))
+          pygame.draw.line(screen1, consts.green, (0, i), (consts.screen_w, i))
+          i += consts.pixel
+
+  for row in range (consts.matriz_rows):
+      for col in range (consts.matriz_cols):
+          if field1[row][col] == "mine" and field1[row][col-1] == "mine" and field1[row][col+1] == "mine" and (field1[row][col+2] == "x" or field1[row][col-2] == "x") :
+              screen1.blit(pygame.transform.smoothscale(consts.mine, (consts.pixel*3, consts.pixel)), (col * consts.pixel, row * consts.pixel)) ,(1400 , 700)
+  pygame.display.update()
+  return screen1
 #---------------------------------------------------------------------------------------------------------------------------------------
+field = game_field.create_regular_field()
+screen = pygame.display.set_mode((consts.screen_w, consts.screen_h))
 def show_screen():
-    screen = pygame.display.set_mode((consts.screen_w, consts.screen_h))
-    screen.fill((consts.green))
-    mainLoop = True
-    count=0
-    field = game_field.create_regular_field()
-    for y in range(len(field)):
-        for x in range(len(field[y])):
-            if field[y][x] == "grass":
-                count += 1
-                screen.blit(pygame.transform.smoothscale(consts.grass, (40, 40)), (y*40 , x*20 ))
-    while mainLoop:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                mainLoop = False
-        pygame.display.update()
-    pygame.quit()
+   screen.fill(consts.green)
+   for y in range(len(field)):
+       for x in range(len(field[y])):
+           if field[y][x] == "grass":
+               screen.blit(pygame.transform.smoothscale(consts.grass, (consts.pixel, consts.pixel)), (x*consts.pixel , y*consts.pixel ))
+   pygame.display.update()
+   return screen
 #---------------------------------------------------------------------------------------------------------------------------------------
-show_screen()
-show_mines()
+# show_screen()
+# show_mines()
+
