722b050fd3db349ccf14dd3150aede6e5e090628
danielkagan-py <danielkagan00@gmail.com>
2026-08-17 11:45:33 +0300

    Update consts.py

--- consts.py
+++ consts.py
@@ -1,21 +1,22 @@
 import pygame
-
+#---------------------------------------------------------------------------------------------------------------------------------------
+pixel=28
 matriz_rows=25
 matriz_cols=50
 ROWS = 25
 COL = 50
 RUNNING_STATE=""
 green = (82, 167, 54)
-screen_w = 1400
-screen_h = 700
+screen_w = pixel*COL
+screen_h = pixel*ROWS
 GREEN = (245, 245, 220)
 black = (0, 0, 0)
-WINDOW_HEIGHT = 700
-WINDOW_WIDTH = 350
 #---------------------------------------------------------------------------------------------------------------------------------------
 WIN_MESSAGE = "You win!"
 LOSE_MESSAGE = "You lose!"
 mine = pygame.image.load("pics/mine.png")
 player = pygame.image.load("pics/soldier.png")
+night_player = pygame.image.load("pics/soldier_nigth.png")
 flag = pygame.image.load("pics/flag.png")
 grass=pygame.image.load("pics/grass.png")
+
